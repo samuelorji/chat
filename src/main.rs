5ac9a29970fddd811450a8a331ac76d14aba7fc0
Samuel Orji <awesomeorji@gmail.com>
2025-10-07 11:31:32 +0100

    set up std arc mutex in a way that the compiler doesn't think its held across await

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use futures::{SinkExt, StreamExt, stream::Any};
 use std::{
-    collections::HashSet, error::Error, fmt::format, net::SocketAddr, ops::Index, sync::Arc,
+    collections::HashSet, error::Error, fmt::format, net::SocketAddr, ops::Index, sync::{Arc, Mutex},
 };
 
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpSocket, TcpStream, tcp::WriteHalf},
     sync::{
-        Mutex,
         broadcast::{self, Sender},
     },
 };
@@ -19,14 +18,14 @@ const HELP_MSG: &str = include_str!("help.txt");
 async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:3000").await?;
 
-    let names = Arc::new(Mutex::new(HashSet::<String>::new()));
+    let names : Arc<Mutex<HashSet<String>>> = Arc::new(Mutex::new(HashSet::<String>::new()));
 
     // create broadcast channel
     let (tx, _) = broadcast::channel::<String>(32);
 
     loop {
         let (connection, a) = server.accept().await?;
-        let namez = names.clone();
+        let namez = Arc::clone(&names);
         tokio::spawn(process_connection(connection, a, tx.clone(), namez));
     }
 
@@ -81,26 +80,33 @@ async fn process_connection(
 
                 _ => {
 
+                    
                     if msg.starts_with("/name ") {
                         if let Some(user_name) =  msg.strip_prefix("/name ") {
-                            {
-                                let mut names_set = names.lock().await;
-                                let not_exists = names_set.insert(user_name.to_string());
-                                if not_exists {
+                            // because we don't want the mutex to be `possibly` held across await,
+                            // we do stuff with it and drop it. 
+                            // if this mutex was locked and then used in an if else statement that contains an await in either the if or the else
+                            // irrespective of if the mutex is explicitly dropped before the call to await in either the if or else blocks .....
+                            // then the compiler will throw a fit, seeing that since both branches could have interacted with the mutex, then the scope of the mutex
+                            // lasts till the end of the else block which makes it look like the mutex is held across an await
+                            let was_inserted = {
+                                let mut names_set = names.lock().unwrap();
+                                let was_inserted = names_set.insert(user_name.to_string());
+                                if was_inserted {
                                     names_set.remove(&name);
-                                    drop(names_set);
-                                    let new_user_name = user_name.to_string();
-                                    let message = format!("{} is now {}",&name, &new_user_name);
-                                    broadcast_sender.send(message)?;
-                                    name = new_user_name
-
-
-                                } else {
-                                    drop(names_set);
-                                    writer.send(format!("name {} already exists", user_name)).await?;
                                 }
-                             }
+                                was_inserted
+                            };
+                            if was_inserted {
+                                let new_user_name = user_name.to_string();
+                                let message = format!("{} is now {}",&name, &new_user_name);
+                                broadcast_sender.send(message)?;
+                                name = new_user_name
+                            } else {
+                                writer.send(format!("name {} already exists", user_name)).await?;
                             }
+                        }
+                            
 
                     } else {
                         msg.push_str(" ❤️!");
